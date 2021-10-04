dbf8cc7491d709f3096bf79e57a9f868a3ca29ab
thaAL <thauandealbuquerqueol@gmail.com>
2021-10-04 14:48:11 -0300

    iniciando o roda pé

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,3 +8,19 @@ function abrirMenu(){
 function fecharMenu(){
   menu.style.width = "0em";
 }
+
+function ativaScrollSuave(selector) {
+
+  $(selector).click(function(event){
+      event.preventDefault();
+
+      var target = $(this).attr('href');
+
+      $('html, body').animate({
+          scrollTop: $(target).offset().top
+      }, 1000);  
+  });
+}
+ativaScrollSuave('a[href*=panel-sobre]');
+ativaScrollSuave('a[href*=panel-palestrante]');
+ativaScrollSuave('a[href*=panel-form]');
