26e47b3394fa74e3ddba50ed8c5ad0f3ed4dacee
thaAL <thauandealbuquerqueol@gmail.com>
2021-10-14 15:16:25 -0300

    atualização

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,43 +1,13 @@
 
- let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
+ let abrir = document.querySelector(".sidebar");
+  let fechar = document.querySelector("#btn");
+  
 
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();//calling the function(optional)
+  fechar.addEventListener("click", ()=>{
+    abrir.classList.toggle("open");
+    menuBtnChange();
   });
 
-  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-  });
-
-  // following are the code to change sidebar button(optional)
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-   }
-  }
-
-  function ativaScrollSuave(selector) {
-
-    $(selector).click(function(event){
-        event.preventDefault();
-
-        var target = $(this).attr('href');
-
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 1000);  
-    });
-}
-
-ativaScrollSuave('a[href*=#img]');
-ativaScrollSuave('a[href*=#sobre-nos]');
-ativaScrollSuave('a[href*=#Cosultas]');
-ativaScrollSuave('a[href*=#consultas-online]');
-ativaScrollSuave('a[href*=#Colaboradores]');
-ativaScrollSuave('a[href*=#panel-form]');
+  
+ 
+  
